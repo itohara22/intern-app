01a9617b203f95c459afd7fae257d86e613d6a93
itohara22 <ito.k23018@gmail.com>
2023-08-17 14:25:58 +0530

    added smoth scrolling

--- src/components/ScrollCards.jsx
+++ src/components/ScrollCards.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const ScrollCard = () => {
   return (
-    <>
+    <div id="emotions">
       <h2 className="text-4xl mt-4 mb-10 px-8 font-bold">
         Lorem ipsum dolor sit.
       </h2>
-      <div className=" flex overflow-x-scroll pb-10 hide-scroll-bar">
+      <div className=" flex overflow-x-scroll p-10 hide-scroll-bar">
         <div className="flex flex-nowrap">
           <div className="inline-block px-3">
-            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2">
+            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2 hover:rotate-[2deg] transition-all duration-75">
               <Image
                 src={"/emoji.svg"}
                 width={50}
@@ -25,7 +25,7 @@ const ScrollCard = () => {
             </div>
           </div>
           <div className="inline-block px-3">
-            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2">
+            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2 hover:rotate-[2deg] transition-all duration-75">
               <Image
                 src={"/emoji.svg"}
                 width={50}
@@ -41,7 +41,7 @@ const ScrollCard = () => {
             </div>
           </div>
           <div className="inline-block px-3">
-            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2">
+            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2 hover:rotate-[2deg] transition-all duration-75">
               <Image
                 src={"/emoji.svg"}
                 width={50}
@@ -57,23 +57,7 @@ const ScrollCard = () => {
             </div>
           </div>
           <div className="inline-block px-3">
-            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2">
-              <Image
-                src={"/emoji.svg"}
-                width={50}
-                height={50}
-                className="mb-6"
-              />
-              <h4 className="font-bold mb-4 text-lg">Lorem, ipsum dolor.</h4>
-              <p className="text-sm leading-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam dicta ipsam est rerum aliquid a? Lorem ipsum dolor
-                sit amet.
-              </p>
-            </div>
-          </div>
-          <div className="inline-block px-3">
-            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2">
+            <div className="bg-orange-200 p-10 w-[40ch] rounded-3xl m-2 hover:rotate-[2deg] transition-all duration-75">
               <Image
                 src={"/emoji.svg"}
                 width={50}
@@ -90,7 +74,7 @@ const ScrollCard = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
